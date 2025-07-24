142249804078fac3d423d06c7713608716d9caf1
Екатерина Швец <shvetses@pik.ru>
2025-07-24 10:28:58 +0300

    commit

---
+++ Example_01/BoolToYesNoConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Data;
+
+namespace Example_01
+{
+    public class BoolToYesNoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? "Да" : "Нет";
+            }
+            return "Нет";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
